6f0f6cc9402438703bdf14ab362d5c47950534f4
mattquan <quan.matthew1999@gmail.com>
2016-06-19 17:15:21 -0400

    Um i think this is the new update...

--- app/src/main/java/com/example/projectyoloswag/wifi/MainActivity.java
+++ app/src/main/java/com/example/projectyoloswag/wifi/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,23 +28,21 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Snackbar.make(view, "Updated", Snackbar.LENGTH_LONG).show();
+                update();
             }
         });
-
         update();
 
     }
 
     public void update(){
-        Handler handler = new Handler(Looper.myLooper());
-        Runnable update=new Runnable() {
-            public void run() {
+
                 WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
                 WifiInfo wifi = wifiManager.getConnectionInfo();
                 String bssid = wifi.getBSSID();
                 int rssi = wifi.getRssi(); //signal strength in dBm
+                Log.e("updated","up");
                 int freq = wifi.getFrequency(); //frequency in MHz
                 TextView bssidView = (TextView) findViewById(R.id.bssid);
                 TextView rssiView = (TextView) findViewById(R.id.rssi);
@@ -51,11 +50,9 @@ public class MainActivity extends AppCompatActivity {
                 bssidView.setText(bssid);
                 rssiView.setText(rssi + " ");
                 freqView.setText(freq + " ");
-            }
-        };
-        for(int i = 0; i < 20; i ++){
-            handler.postDelayed(update, 1000);
-        }
+
+
+
     }
 
 
